d9c768d5f9dbb2672d392d0c67e31e1a0120ca40
dnorkett <dnorkett@gmail.com>
2020-05-25 00:28:26 -0500

    added user authentication modules

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');           //package to hide sensitive info
+
 //Import Routes
 const authRoute = require('./routes/auth');
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,25 @@
 const router = require('express').Router();
-const userModel = require('../model/User');
+const User = require('../model/User');
+const {registerValidation, loginValidation} = require('../validation');
 
+router.post('/register', async (req, res) => {          
+    //Validate data before making a user    
+    const {error} = registerValidation(req.body);    
+    if (error) return res.status(400).send(error.details[0].message);
 
-//Everything in this route is relative to /api/user
-router.post('/register', async (req, res) => {
-    const user = new user({
+    //Checking if the user is already in the database
+    const emailExists = await User.findOne({email: req.body.email});
+    if (emailExists) return res.status(400).send('Email alreadt exists');
+    
+    //If valid information, create user
+    const user = new User({
         name: req.body.name,
         email: req.body.email,
         password: req.body.password
     });
     try {
         const savedUser = await user.save();
+        res.send(savedUser);
     }catch(err){
         res.status(400).send(err);
     }
