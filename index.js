68f593e67b8d259a15c5c382053e6e6c6a14fa1e
dnorkett <dnorkett@gmail.com>
2020-05-25 11:28:32 -0500

    completed authentication module

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv');           //package to hide sensitive info
 
 //Import Routes
 const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use(express.json());
 
 //Route Middlewar
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 
 app.listen(3000, () => console.log('Server running'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation, loginValidation} = require('../validation');
+const jwt = require('jsonwebtoken');
 
+
+//CREATE USER
 router.post('/register', async (req, res) => {          
     //Validate data before making a user    
     const {error} = registerValidation(req.body);    
@@ -9,13 +13,17 @@ router.post('/register', async (req, res) => {
 
     //Checking if the user is already in the database
     const emailExists = await User.findOne({email: req.body.email});
-    if (emailExists) return res.status(400).send('Email alreadt exists');
+    if (emailExists) return res.status(400).send('Email already exists');
+    
+    //Hash password so we don't store in plain text
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     
     //If valid information, create user
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashedPassword
     });
     try {
         const savedUser = await user.save();
@@ -23,6 +31,29 @@ router.post('/register', async (req, res) => {
     }catch(err){
         res.status(400).send(err);
     }
+});
+
+
+//LOGIN
+router.post('/login', async (req, res) => {
+    //Validate data before attempting to login
+    const {error} = loginValidation(req.body);    
+    if (error) return res.status(400).send(error.details[0].message);
+
+    //Checking if the email exists
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email not found');
+
+    //Checking if the password is correct
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    if (!validPass) return res.status(400).send('Password is incorrect');
+
+    //Create and assign a token
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+    res.header('auth-token', token).send(token);
+    
+    
+    
 });
 
 
